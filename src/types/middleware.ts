9525a996b3cc95edd91db9329f47a528a85cc1bf
Márcio DEV <playdznvlt@gmail.com>
2026-02-12 21:53:21 -0300

    feat: update version to 0.0.15 and add RouterGlobalData to middleware types

--- src/types/middleware.ts
+++ src/types/middleware.ts
@@ -1,5 +1,5 @@
 import { RouterMiddleware } from '@/components/Middleware.js';
-import { RouterGlobalRequest, RouterGlobalResponse } from '@/components/RouteSchema.js';
+import { RouterGlobalData, RouterGlobalRequest, RouterGlobalResponse } from '@/components/RouteSchema.js';
 
 export type RouterMiddlewareClass<T extends RouterMiddleware = RouterMiddleware> = {
 	new (...args: any[]): T;
@@ -15,9 +15,14 @@ export type MountedMiddleware<T = any> = {
 
 export type MiddlewareVariant = RouterMiddlewareClass | MountedMiddleware;
 
-export interface RouterMiddlewareContext<SRequest = RouterGlobalRequest, SResponse = RouterGlobalResponse> {
+export interface RouterMiddlewareContext<
+	SRequest = RouterGlobalRequest,
+	SResponse = RouterGlobalResponse,
+	SData = RouterGlobalData
+> {
 	request: SRequest;
 	response: SResponse;
+	data: SData;
 
 	body: any;
 	query: any;
